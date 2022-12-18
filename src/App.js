34c590ae39e5f707bfcef6ddd1f05291cf94c3bd
mahesh <maheshkhare87@gmail.com>
2022-12-18 17:43:01 +0530

    multiline tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-// JSX with multiline but single tag
-function App(){
-  return <h1></h1>;
+//multiline tags
+//since every function is a tag,thats why we are calling by using tag
+function App() {
+  return (
+    <div>
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+    </div>
+  );
+}
+
+// <Person />
+function Person() {
+  return (
+    <div>
+      <img src="https://picsum.photos/400" alt="" />
+      <h1>Person Name </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
+        delectus placeat? Consequuntur iusto perferendis voluptatibus, ipsa sit
+        non. Quo cumque veniam nostrum voluptatem qui accusantium ipsa labore
+        libero! Tempore, consequuntur!
+      </p>
+    </div>
+  );
 }
 
 export default App;
