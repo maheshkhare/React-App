2da5bfae9e9e7a7b3ca356897a0954fca6220c84
mahesh <maheshkhare87@gmail.com>
2022-12-19 22:57:54 +0530

    interpolation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-//multiline tags
-//since every function is a tag,thats why we are calling by using tag
+//USER DEFINED TAGS==COMPONENT
 function App() {
-  return (
-    <div>
-      <Person />
-      <Person />
-      <Person />
-      <Person />
-      <Person />
-      <Person />
-      <Person />
-    </div>
-  );
-}
+  let project = "My React Project"; //local variables
+  let username = "Mahesh";
 
-// <Person />
-function Person() {
+  //JSX => HTML CSS
+  //by using tag we accessed the local varibles and returned
   return (
     <div>
-      <img src="https://picsum.photos/400" alt="" />
-      <h1>Person Name </h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
-        delectus placeat? Consequuntur iusto perferendis voluptatibus, ipsa sit
-        non. Quo cumque veniam nostrum voluptatem qui accusantium ipsa labore
-        libero! Tempore, consequuntur!
-      </p>
+      <h1>{project}</h1>
+      <h1>{username}</h1>
     </div>
   );
 }
