a07b9250094d8348ba854d7b2e25f3d548ebadf1
mahesh <maheshkhare87@gmail.com>
2022-12-20 01:44:44 +0530

    map

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 function App() {
   let title = "Map Demo"; //varible
-  let user = { id: 1, city: "mumbai" }; //object
+  let list = ["mumbai", "pune"]; //object
 
   return (
     <div>
       <h1>{title}</h1>
-      <h1>{user.city}</h1>
+      {list.map((item) => (
+        <h1>{item}</h1>
+      ))}
     </div>
   );
 }
 
 export default App;
 
-//user.city........this function will mapping the values
+// [delhi, mumbai] => [<h1>delhi</h1>, <h1>mumbai</h1>]
+//map fn creates a varibale item,inside it we have store the list,and by using item we have printing one/one
+//element from the list
