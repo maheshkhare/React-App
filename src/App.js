0bd7443d635381de896c4f5a9ea8caf3d9e71911
mahesh <maheshkhare87@gmail.com>
2022-12-20 02:01:23 +0530

    map with cloning

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import { useState } from "react";
+
 function App() {
-  let title = "Map Demo"; //varible
-  let list = ["mumbai", "pune"]; //object
+  let title = "Map Demo";
+  //initialize list=empty list
+  let [list, setList] = useState([]); //stateful variable
+
+  //member function
+  let addItem = () => {
+    //logical part,cloning the list item and adding one insiide newList
+    let newList = [...list, "Pandharpur"];
+
+    //dom part,set all updated data in list
+    setList(newList);
+  };
 
   return (
     <div>
       <h1>{title}</h1>
+      <input type="button" value="Add Item into List" onClick={addItem} />
+      {/* after clicking on button call given to the function  */}
+
       {list.map((item) => (
         <h1>{item}</h1>
       ))}
+      {/* printing one/one ele. from the list */}
     </div>
   );
 }
 
 export default App;
-
-// [delhi, mumbai] => [<h1>delhi</h1>, <h1>mumbai</h1>]
-//map fn creates a varibale item,inside it we have store the list,and by using item we have printing one/one
-//element from the list
