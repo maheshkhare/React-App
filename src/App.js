fc961dbf76c34e1a4d88c372b35e2c11c41099b1
mahesh <maheshkhare87@gmail.com>
2022-12-20 01:29:51 +0530

    map function

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import { useState } from "react";
-
 function App() {
-  // useState, hooks, Stateful Variable
-  // ES6 Syntax : De-Structureing
-  // let counter = 100;
-  let [counter, setCounter] = useState(100);
-
-  // Member function,
-  // ES6 Syntax: Arrow Function
-  // React Binding + using Interpolation
-  // How calling this function.
-  let increment = () => {
-    // logical opr
-    counter++;
-
-    // dom opr
-    setCounter(counter);
-  };
+  let title = "Map Demo"; //varible
+  let user = { id: 1, city: "mumbai" }; //object
 
   return (
     <div>
-      <h1>Counter Application</h1>
-      <h1> {counter} </h1>
-      <input type="button" value="Incrment" onClick={increment} />
+      <h1>{title}</h1>
+      <h1>{user.city}</h1>
     </div>
   );
 }
 
 export default App;
+
+//user.city........this function will mapping the values
