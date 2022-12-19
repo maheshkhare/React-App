53b5d7e5da7e967b8e0accc0544b5daf00b3a472
mahesh <maheshkhare87@gmail.com>
2022-12-20 01:04:14 +0530

    ES6 Basic

--- src/App.js
+++ src/App.js
@@ -1,49 +1,30 @@
-//Statefull variable
-//we import useState function from react library
 import { useState } from "react";
 
 function App() {
-  let str1 = "Home";
-  let str2 = "Explore";
-  let str3 = "Notifications";
-  //let set[initilize] the counter value=1
-  let [counter, setCounter] = useState(1);
-  //initilize city=mulbai
-  let [city, SetCity] = useState("mumbai");
+  // useState, hooks, Stateful Variable
+  // ES6 Syntax : De-Structureing
+  // let counter = 100;
+  let [counter, setCounter] = useState(100);
 
-  //Arrow of function
+  // Member function,
+  // ES6 Syntax: Arrow Function
+  // React Binding + using Interpolation
+  // How calling this function.
   let increment = () => {
-    // giving logic
+    // logical opr
     counter++;
 
-    //DOM UPDATE PART >>RE_RANDRERING
+    // dom opr
     setCounter(counter);
-  };
-
-  //Arrow of function
-  let changeCity = () => {
-    city = "Hello " + city;
-
-    //re-rendering the value of city
-    SetCity(city);
   };
 
   return (
     <div>
-      <h1>{city}</h1>
-      <input type="button" value="Change City" onClick={changeCity} />
-      <hr />
-      <h1>{str1}</h1>
-      <h1>{str2}</h1>
-      <h1>{str3}</h1>
-      <h1>{counter}</h1>
-      <input type="button" value="Increment" onClick={increment} />
+      <h1>Counter Application</h1>
+      <h1> {counter} </h1>
+      <input type="button" value="Incrment" onClick={increment} />
     </div>
   );
 }
 
 export default App;
-
-//by using the stateful[useState] variable we can change the values of variables
-//like in twiter,facebbok,whatsapp there is some stable and some unstable options are there
-//
