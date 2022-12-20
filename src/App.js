0fde740cb77388fcdd413770412a92da841abcad
mahesh <maheshkhare87@gmail.com>
2022-12-20 19:48:12 +0530

    map-demo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import { useState } from "react";
 
 function App() {
-  // set default message as =ABC
-  let [message, setMessage] = useState("Abc"); //datamember
-
-  // member fn
-  let updateMessage = (e) => {
-    // e.target == document.querySelector("input")
-    //first state is equal to second statement[querySelector]
-    message = e.target.value;
-    setMessage(message);
-  };
+  let [list] = useState(["pandharpur", "pune", "Solapur"]);
 
   return (
     <div>
-      <h1>Working With Live Input</h1>
+      <h1>Map Demo</h1>
 
-      <input
-        type="text"
-        placeholder="Enter Your Text"
-        value={message} //this line only shows default value on textbox==ABC
-        onChange={updateMessage} //when we type on textbox,then fn call given and all changes saved in msg
-      />
-
-      <h1>{message}</h1>
-      {/* and this show over here */}
+      {list.map((item) => (
+        <div className="alert alert-primary">{item}</div>
+      ))}
     </div>
+    // by using map all ele from datamember featched and returned,return call given to the app.html
   );
 }
 
