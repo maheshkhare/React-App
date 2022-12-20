2469b85f2128e9cef8a474870e5d3ab0202c6bac
mahesh <maheshkhare87@gmail.com>
2022-12-20 17:00:26 +0530

    Theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
+import { useState } from "react";
+
 function App() {
+  // initialized default theme = primary
+  let [theme, setTheme] = useState("primary"); //data member
+
+  // member Function's
+  let makePrimaryTheme = () => {
+    // this is bootstrap properties
+    theme = "primary";
+
+    // RE-RANDRING
+    setTheme(theme);
+  };
+
+  let makeSuccessTheme = () => {
+    theme = "success";
+
+    // RE-RANDRING
+    setTheme(theme);
+  };
+
+  let makeDangerTheme = () => {
+    theme = "danger";
+
+    // RE-RANDRING
+    setTheme(theme);
+  };
+
   return (
     <div>
-      <h1 className="bg-dark text-light p-2">BootStrap Styling!!!</h1>
+      {/* after clicking on button result set in below this datamemer and it is applied on string */}
+      {/* by-default theme we have declared as primary */}
+      <h1 className={`bg-${theme} text-light p-3`}>Bootstrap Styling</h1>
 
-      <h1 className="bg-success" style={{ color: "//#endregion" }}>
-        Hello Mahi
-      </h1>
+      <input
+        type="button"
+        value="primary Theme"
+        className="btn btn-primary"
+        onClick={makePrimaryTheme}
+      />
+
+      <input
+        type="button"
+        value="success Theme"
+        className="btn btn-success"
+        onClick={makeSuccessTheme}
+      />
+
+      <input
+        type="button"
+        value="Danger Theme"
+        className="btn btn-danger"
+        onClick={makeDangerTheme}
+      />
     </div>
   );
 }
