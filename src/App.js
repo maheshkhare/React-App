0001b16c2d73aca5f1c3715bee58e043b81e3e93
mahesh <maheshkhare87@gmail.com>
2022-12-20 19:40:15 +0530

    live uppdate

--- src/App.js
+++ src/App.js
@@ -1,78 +1,32 @@
 import { useState } from "react";
 
 function App() {
-  // we have stored 9 elements in list
-  let [list] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1]);
-  // initialized default theme = primary,stateful variable
-  let [theme, setTheme] = useState("primary"); //data member
+  // set default message as =ABC
+  let [message, setMessage] = useState("Abc"); //datamember
 
-  // member Function's
-  let makePrimaryTheme = () => {
-    // this is bootstrap properties
-    theme = "primary";
-
-    // RE-RANDRING
-    setTheme(theme);
-  };
-
-  let makeSuccessTheme = () => {
-    theme = "success";
-
-    // RE-RANDRING
-    setTheme(theme);
-  };
-
-  let makeDangerTheme = () => {
-    theme = "danger";
-
-    // RE-RANDRING
-    setTheme(theme);
+  // member fn
+  let updateMessage = (e) => {
+    // e.target == document.querySelector("input")
+    //first state is equal to second statement[querySelector]
+    message = e.target.value;
+    setMessage(message);
   };
 
   return (
     <div>
-      <div className="sticky-top">
-        {/* after clicking on button theme will apply here */}
-        <h1 className={`bg-${theme} text-light p-3`}>
-          Bootstrap Styling {theme}
-          {/* last theme will will provide the theme name infront of string */}
-        </h1>
+      <h1>Working With Live Input</h1>
 
-        <input
-          type="button"
-          value="primary Theme"
-          className="btn btn-primary"
-          onClick={makePrimaryTheme}
-        />
+      <input
+        type="text"
+        placeholder="Enter Your Text"
+        value={message} //this line only shows default value on textbox==ABC
+        onChange={updateMessage} //when we type on textbox,then fn call given and all changes saved in msg
+      />
 
-        <input
-          type="button"
-          value="success Theme"
-          className="btn btn-success"
-          onClick={makeSuccessTheme}
-        />
-
-        <input
-          type="button"
-          value="Danger Theme"
-          className="btn btn-danger"
-          onClick={makeDangerTheme}
-        />
-      </div>
-
-      {list.map((item) => (
-        //after clicking on button theme will be apply here,and this will iterate 9 times
-        <div className={`alert alert-${theme} my-1`}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. A eius,
-          voluptas quo, iusto id commodi optio sed perspiciatis natus minima
-          incidunt reprehenderit quos ratione tempora atque vero aspernatur
-          itaque dicta!
-        </div>
-      ))}
+      <h1>{message}</h1>
+      {/* and this show over here */}
     </div>
   );
 }
 
 export default App;
-
-// bootstrap styling
