7f7b4cca16f62b688fc6b7701aebfedeefe0c87c
mahesh <maheshkhare87@gmail.com>
2022-12-20 20:23:37 +0530

    message-app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 function App() {
-  let [list] = useState([
+  // statefull varible
+  let [list, setList] = useState([
     //datamembers
     { message: "Hii mahi", messageTime: new Date() },
     { message: "How r You", messageTime: new Date() },
@@ -9,16 +10,36 @@ function App() {
     { message: "How's project going on", messageTime: new Date() },
   ]);
 
+  // member fn
+  let addMessage = () => {
+    let newMessage = {
+      message: "Chill Santosh Sir..!",
+      messageTime: new Date(),
+    };
+    list = [newMessage, ...list];
+    // save in list new msg+clone privious messages
+
+    setList(list);
+  };
+
   return (
     <div>
       <h1 className="bg-primary text-white p-3">Messaging App</h1>
+
+      <input
+        type="button"
+        value="Add Message"
+        className="btn btn-primary"
+        onClick={addMessage}
+        // when we click button then hardcoded message added on web
+      />
 
       {list.map((item, index) => (
         // index not use here
         <div key={index} className="d-flex my-1">
           <div className="badge text-bg-primary">
             {item.message}
-            <span className="ms-3">
+            <span className="ms-4">
               {/* margine ms-3 for time */}
               {item.messageTime.getHours()}:{item.messageTime.getMinutes()}
             </span>
