7689ea1fb81e782297d56ef4ec521edf2fe2e0f5
mahesh <maheshkhare87@gmail.com>
2022-12-20 16:19:42 +0530

    Global Css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import "./App.css";
-
 function App() {
-  let mystyle = { color: "white", background: "purple" };
   return (
     <div>
-      {/* This kind of External Styling */}
-      <h1 className="beautify">Hello Again!</h1>
-
-      {/* This kind of internal Styling NOTE:ONLY ONE CURLY BRACES */}
-      <h1 style={mystyle}>Hello Mahi</h1>
-
-      {/* THIS IS INLINE STYLING */}
-      <h1 style={{ color: "white", backgroundColor: "red" }}>Hello</h1>
+      <h1 className="global">Hello Global World</h1>
     </div>
   );
 }
 
 export default App;
 
-//it that project we have seen 3 types of stylings
+// Global External css
