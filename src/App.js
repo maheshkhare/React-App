592fd603a74d80e6fe4c33f0d278307c4c7a44c4
mahesh <maheshkhare87@gmail.com>
2022-12-20 20:06:29 +0530

    hard-coded messaging app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import { useState } from "react";
 
 function App() {
-  let [list] = useState(["pandharpur", "pune", "Solapur"]);
+  let [list] = useState([
+    //datamembers
+    { message: "Hii mahi", messageTime: new Date() },
+    { message: "How r You", messageTime: new Date() },
+    { message: "I'm doing labwork", messageTime: new Date() },
+    { message: "How's project going on", messageTime: new Date() },
+  ]);
 
   return (
     <div>
-      <h1>Map Demo</h1>
+      <h1 className="bg-primary text-white p-3">Messaging App</h1>
 
-      {list.map((item) => (
-        <div className="alert alert-primary">{item}</div>
+      {list.map((item, index) => (
+        // index not use here
+        <div key={index} className="d-flex my-1">
+          <div className="badge text-bg-primary">
+            {item.message}
+            <span className="ms-3">
+              {/* margine ms-3 for time */}
+              {item.messageTime.getHours()}:{item.messageTime.getMinutes()}
+            </span>
+          </div>
+        </div>
       ))}
     </div>
-    // by using map all ele from datamember featched and returned,return call given to the app.html
   );
 }
-
+// take one/one datamembers and print by using map
 export default App;
