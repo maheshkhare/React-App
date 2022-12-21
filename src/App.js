419438f5b9ca50ee775bb58a37bf1ad73b13bdf8
mahesh <maheshkhare87@gmail.com>
2022-12-22 01:24:48 +0530

    design part

--- src/App.js
+++ src/App.js
@@ -37,7 +37,6 @@ function App() {
 
     let data = {
       message: message,
-      reply: true,
     };
 
     await axios.post(url, data);
@@ -56,27 +55,60 @@ function App() {
 
   return (
     <div>
-      <h1>{title}</h1>
+      <h1 className="bg-dark text-light p-3 sticky-top">{title}</h1>
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-6">
+          <div className="d-flex">
+            <input
+              className="form-control form-control-lg"
+              type="text"
+              placeholder="Hi...whatsapp...!!"
+              value={message} //this is for empty the textbox
+              onChange={handleOnChangeMessage}
+              onKeyUp={checkEnterCode}
+              ref={inputRef} //document.querySelector() for onkeyup
+              required
+              minLength={2}
+            />
 
-      <input
-        type="text"
-        placeholder="Hi...whatsapp...!!"
-        value={message} //this is for empty the textbox
-        onChange={handleOnChangeMessage}
-        onKeyUp={checkEnterCode}
-        ref={inputRef} //document.querySelector() for onkeyup
-        required
-        minLength={2}
-      />
+            <input
+              className="btn btn-secondary"
+              type="button"
+              value="Add"
+              onClick={() => createNewMessage(false)}
+            />
 
-      <input
-        type="button"
-        value="Make Ajax/API POST Call"
-        onClick={createNewMessage}
-      />
+            <input
+              className="btn btn-secondary"
+              type="button"
+              value="Reply"
+              onClick={() => createNewMessage(true)}
+            />
+          </div>
+        </div>
+      </div>
 
       {messageList.map((item, index) => (
-        <div key={index}>{item.message}</div>
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-6">
+            <div
+              key={index}
+              className={
+                item.reply
+                  ? "d-flex justify-content-end my-1" //if value is true then print at the end we are used ternary op
+                  : "d-flex justify-content-start my-1" //if value is false then print at the end
+              }
+            >
+              <div className="badge text-bg-secondary">
+                {item.message}
+                <span className="ms-4">
+                  {new Date(item.messageTime).getHours()};
+                  {new Date(item.messageTime).getMinutes()};
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
