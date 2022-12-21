7f2d75da465ce7f8d232db7f64ab5b93e046855a
mahesh <maheshkhare87@gmail.com>
2022-12-21 14:28:09 +0530

    Ajax Call

--- src/App.js
+++ src/App.js
@@ -1,53 +1,35 @@
+// importing the axios libraries for making Ajax call
+import axios from "axios";
 import { useState } from "react";
 
 function App() {
-  let [message, setMessage] = useState(""); //firstly new typed msg stored here then it is given to the add msg fn
-  let [list, setList] = useState([]); //privious and newly coming msg stored here
+  // Datamembers
+  let [title] = useState("API DEMO");
+  let [messageList, setMessageList] = useState([]);
 
-  // Member fn
-  let updateInputMessage = (e) => {
-    message = e.target.value;
-    setMessage(message); //ontyping time add text is message list
-  };
+  // member fn
+  let getAllMessages = async () => {
+    let url = `http://localhost:3001/messages`;
+    let responce = await axios.get(url); //featching url+data by the axios obj
 
-  let addMessage = () => {
-    let newMessage = { message: message, messageTime: new Date() }; //take message from msg list
-    list = [newMessage, ...list]; //add new msg and privious msgs in list
-    setList(list);
+    // Getting the Message From Server : And Re-rendering
+    messageList = [...responce.data]; //cloning the all data from backend
+    setMessageList(messageList);
   };
 
   return (
     <div>
-      <h1 className="bg-primary text-white p-3">Messaging Demo</h1>
+      <h1>{title}</h1>
 
-      <div className="d-flex">
-        <input
-          className="form-control"
-          value={message}
-          id="textId1"
-          onChange={updateInputMessage} //live typing time call given
-          type="text"
-          placeholder="Enter Message"
-        />
-        <input
-          className="btn btn-primary"
-          type="button"
-          value="Add Message"
-          onClick={addMessage} //after clicking button
-        />
-      </div>
+      <input
+        type="button"
+        value="Make Ajax/API Call"
+        onClick={getAllMessages}
+      />
 
-      {/* take one/one messages from list and print on web page */}
-
-      {list.map((item, index) => (
-        <div key={index} className="d-flex my-1">
-          <div className="badge text-bg-primary">
-            {item.message}
-            <span className="ms-4">
-              {item.messageTime.getHours()}:{item.messageTime.getMinutes()}
-            </span>
-          </div>
-        </div>
+      {messageList.map((item) => (
+        <div>{item.message}</div>
+        // message is the element of backend
       ))}
     </div>
   );
