cdb5ec0f31de8e2cfa9c9508f0535063448d4c09
mahesh <maheshkhare87@gmail.com>
2022-12-22 16:00:48 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import AppBody from "./component/AppBody";
-import AppFooter from "./component/AppFooter";
-import AppHeader from "./component/AppHeader";
+import { Route, Routes } from "react-router-dom";
 
-// Main fn
 function App() {
   return (
-    <div>
-      <AppHeader />
-      <AppBody />
-      <AppBody />
-      <AppFooter />
-    </div>
+    <Routes>
+      {/** http://localhost:3000/ */}
+      <Route path="/" element={<h1>Home</h1>} />
+
+      {/** http://localhost:3000/home */}
+      <Route path="/home" element={<h1>Home</h1>} />
+
+      {/** http://localhost:3000/explore */}
+      <Route path="/explore" element={<h1>Explore</h1>} />
+
+      {/** http://localhost:3000/notification */}
+      <Route path="/notification" element={<h1>Notification</h1>} />
+
+      <Route path="*" element={<h1>Page Not Found</h1>} />
+    </Routes>
   );
 }
 
